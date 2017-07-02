26245eef81922beed0a38cddd23ff85bf80ecd33
wk645 <wk645@nyu.edu>
2017-07-02 11:10:49 -0400

    inheritance intro

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+
+# subclasses
+
+class Car < Vehicle
+	# inherits the Car class from Vehicle
+	def go
+		"VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+	end
+	# overwrite
+end
